fbc123268ae1dc0130fdb1289847edb1ba938301
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-22 10:33:43 +0800

    Display fetched user data

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,17 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
 
 function UsersList() {
 
     const dispatch = useDispatch();
 
+    const { isLoading, data, error } = useSelector((state) => {
+        return state.users;
+    });
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, []);
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
+    if (error) {
+        return <div>Error fetching data...</div>;
+    }
+
     return (
-        <div>UsersList</div>
+        <div>{data.length}</div>
     );
 }
 
