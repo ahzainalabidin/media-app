c0c52f3664e08342674ac388a376f973ed5dbd55
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-22 10:03:34 +0800

    Fetch user when first rendered

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchUsers } from "../store";
+
 function UsersList() {
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
     return (
         <div>UsersList</div>
     );
