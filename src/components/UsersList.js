67b603d7eb6c5d87bc2e5a7bd303f430a046a94d
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-22 11:53:57 +0800

    Display user name

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -23,8 +23,20 @@ function UsersList() {
         return <div>Error fetching data...</div>;
     }
 
+    const renderedUsers = data.map((user) => {
+
+        return (
+            <div key={user.id} className="mb-2 border rounded">
+                <div className="flex p-2 justify-between items-center cursor-pointer">
+                    {user.name}
+                </div>
+            </div>
+        );
+
+    });
+
     return (
-        <div>{data.length}</div>
+        <div>{renderedUsers}</div>
     );
 
 }
