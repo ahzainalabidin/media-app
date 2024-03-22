58c70a5d649247e9152e1e21ae327f1202c93a95
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-22 14:33:12 +0800

    Move loading and error state to components

--- src/components/UsersList.js
+++ src/components/UsersList.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addUser, fetchUsers } from "../store";
 import Skeleton from "./Skeleton";
@@ -6,25 +6,32 @@ import Button from "./Button";
 
 function UsersList() {
 
+    const [isLoadingUsers, setIsLoadingUsers] = useState(false);
+    const [loadingUsersError, setLoadingUsersError] = useState(null);
+
     const dispatch = useDispatch();
 
-    const { isLoading, data, error } = useSelector((state) => {
+    const { data } = useSelector((state) => {
         return state.users;
     });
 
     useEffect(() => {
-        dispatch(fetchUsers());
+        setIsLoadingUsers(true);
+        dispatch(fetchUsers()).
+            unwrap()
+            .catch((err) => setLoadingUsersError(err))
+            .finally(() => setIsLoadingUsers(false));
     }, []);
 
     const handleUserAdd = () => {
         dispatch(addUser());
     };
 
-    if (isLoading) {
+    if (isLoadingUsers) {
         return <Skeleton times={6} className="h-10 w-full" />;
     }
 
-    if (error) {
+    if (loadingUsersError) {
         return <div>Error fetching data...</div>;
     }
 
