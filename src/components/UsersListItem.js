ab7b72022271fb44d734d93d1269b3a0d89fb3f8
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-25 08:35:13 +0800

    Style name to justify to the left

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -14,14 +14,16 @@ function UsersListItem({ user }) {
     return (
         <div className="mb-2 border rounded">
             <div className="flex p-2 justify-between items-center cursor-pointer">
+                <div className="flex flex-row items-center justify-between">
 
-                <Button loading={isLoading} onClick={handleClick}>
-                    <GoTrash />
-                </Button>
+                    <Button className="mr-3" loading={isLoading} onClick={handleClick}>
+                        <GoTrash />
+                    </Button>
 
-                {error && <div>Error deleting user.</div>}
-                {user.name}
+                    {error && <div>Error deleting user.</div>}
+                    {user.name}
 
+                </div>
             </div>
         </div>
     );
