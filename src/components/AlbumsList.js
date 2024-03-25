a9528dce632cd4f46935a25f883fc67de1062a94
ahzainalabidin <162523810+ahzainalabidin@users.noreply.github.com>
2024-03-25 13:19:13 +0800

    Add album list

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -1,14 +1,38 @@
 import { useFetchAlbumsQuery } from "../store";
+import ExpandablePanel from "./ExpandablePanel";
+import Skeleton from "./Skeleton";
 
 function AlbumsList({ user }) {
 
     const { data, error, isLoading } = useFetchAlbumsQuery(user);
 
-    console.log(data, error, isLoading);
+    let content;
+
+    if (isLoading) {
+        content = <Skeleton times={3} className="h-10 w-full" />;
+    } else if (error) {
+        content = <div>Error loading albums...</div>;
+    } else {
+        content = data.map((album) => {
+            const header = <div>{album.title}</div>
+            return (
+                <ExpandablePanel key={album.id} header={header}>
+                    List of photos in the album
+                </ExpandablePanel>)
+        });
+    }
 
     return (
         <div>
-            Albums for {user.name}
+
+            <div>
+                Albums for {user.name}
+            </div>
+
+            <div>
+                {content}
+            </div>
+
         </div>
     );
 
